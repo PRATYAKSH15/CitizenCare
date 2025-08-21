6d6fd5f5c76422d6af7cf445b17807e2b1b70545
Ayush-Shukla333 <ayushshukla1126@gmail.com>
2025-08-21 11:01:21 +0530

    Added paragraph

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
+      <p>This is our project called CitizenCare</p>
     </div>
   )
 }
